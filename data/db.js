14274f269a6100d950e508a2cbbe0e8527c9670f
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-28 16:09:02 +0200

    [UPD] Add database connection

---
+++ data/db.js
@@ -0,0 +1,16 @@
+const mysql = require(`mysql2`);
+const password = process.env.DB_PASSWORD;
+
+const connection = mysql.createConnection({
+    host: `localhost`,
+    user: `root`,
+    password,
+    database: `db_blog`
+})
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL!');
+})
+
+module.exports = connection;
