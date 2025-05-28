7b16d5f2c6ebe2c5e33dabd2e4f481861ccc0887
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-28 15:45:04 +0200

    [UPD] Add error handling and not found middleware

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const port = 3000;
 
 const blogRouter = require(`./routers/blogRouter`);
 
+const notFound = require(`./middlewares/notFound.js`);
+const errorsHandler = require("./middlewares/errorsHandler.js");
+
 app.use('/blog', blogRouter);
 
 app.get(`/`, (req, res) => {
     res.send(`Benvenuto`);
 })
+
+app.use(errorsHandler);
+app.use(notFound);
 
 app.listen(port, () => {
     console.log(`Il server è in ascolto`);
